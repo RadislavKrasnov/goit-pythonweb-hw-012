a2e9bbdd3422f088e68c782403defad4e803db48
Radyslav Krasnov <radislavkrasnoff@gmail.com>
2025-06-19 21:54:51 +0300

    add unit tests for contacts service

--- tests/unit/service/test_contacts_service.py
+++ tests/unit/service/test_contacts_service.py
@@ -46,3 +46,61 @@ async def test_create_contact_integrity_error(contact_service, user, contact_dat
 
     contact_service.repository.db.rollback.assert_awaited_once()
     assert exc.type is not None
+
+
+@pytest.mark.asyncio
+async def test_get_contacts(contact_service, contact_data, user):
+    contact_service.repository.get_contacts = AsyncMock(return_value=[contact_data])
+    result = await contact_service.get_contacts(0, 10, user)
+    assert result == [contact_data]
+
+
+@pytest.mark.asyncio
+async def test_get_contact(contact_service, contact_data, user):
+    contact_service.repository.get_contact_by_id = AsyncMock(return_value=contact_data)
+    result = await contact_service.get_contact(1, user)
+    assert result == contact_data
+
+
+@pytest.mark.asyncio
+async def test_update_contact_success(contact_service, user, contact_data):
+    contact_service.repository.update_contact = AsyncMock(return_value=contact_data)
+    result = await contact_service.update_contact(1, contact_data, user)
+    assert result == contact_data
+
+
+@pytest.mark.asyncio
+async def test_update_contact_integrity_error(contact_service, user, contact_data):
+    contact_service.repository.update_contact = AsyncMock(
+        side_effect=IntegrityError("mock", {}, None)
+    )
+    contact_service.repository.db.rollback = AsyncMock()
+
+    with pytest.raises(Exception) as exc:
+        await contact_service.update_contact(1, contact_data, user)
+
+    contact_service.repository.db.rollback.assert_awaited_once()
+    assert exc.type is not None
+
+
+@pytest.mark.asyncio
+async def test_delete_contact(contact_service, contact_data, user):
+    contact_service.repository.delete_contact = AsyncMock(return_value=contact_data)
+    result = await contact_service.delete_contact(1, user)
+    assert result == contact_data
+
+
+@pytest.mark.asyncio
+async def test_search_contacts(contact_service, contact_data, user):
+    contact_service.repository.search_contacts = AsyncMock(return_value=[contact_data])
+    result = await contact_service.search_contacts(user, first_name="John")
+    assert result == [contact_data]
+
+
+@pytest.mark.asyncio
+async def test_get_upcoming_birthdays(contact_service, contact_data, user):
+    contact_service.repository.get_upcoming_birthdays = AsyncMock(
+        return_value=[contact_data]
+    )
+    result = await contact_service.get_upcoming_birthdays(user)
+    assert result == [contact_data]
